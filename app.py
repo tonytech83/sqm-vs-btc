a201acf007a041229d9875d9a82112c7c45f13ef
tonytech83 <a.petrov.83@gmail.com>
2024-12-27 15:39:30 +0200

    change the host IP address to 0.0.0.0 and PORT to 5000

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, jsonify
 import requests
 from bs4 import BeautifulSoup
@@ -77,4 +78,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
