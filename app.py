d9de1fc76c6637a4d327e0f77ff40147f72a7988
tonytech83 <a.petrov.83@gmail.com>
2024-12-27 15:53:58 +0200

    Change the way server start

--- app.py
+++ app.py
@@ -78,5 +78,6 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Starting server on port {port}")
     app.run(host="0.0.0.0", port=port)
