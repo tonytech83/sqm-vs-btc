9af7f4164ce2a1c5a551145f52c4b05219509798
tonytech83 <a.petrov.83@gmail.com>
2024-12-30 11:35:52 +0200

    Changes: - Separate calc and fetch function outside Flask app - Made line smooth

--- app.py
+++ app.py
@@ -1,95 +1,17 @@
 import os
 from flask import Flask, render_template, jsonify
-import requests
-from bs4 import BeautifulSoup
 from datetime import datetime
 import json
 
-app = Flask(__name__)
-
-# Configure static file serving
-app.static_folder = "static"
-app._static_url_path = "./static"
+from engin.helpers import get_sqm_price_in_eur, get_btc_price_in_eur, get_prices_and_ratio
 
 
-def get_sqm_price_in_eur():
-    url = "https://www.imoti.net/bg/sredni-ceni"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    price_per_sqm_rows = soup.find("tbody").find_all("tr")
-
-    total_price = 0
-    count = 0
-
-    for row in price_per_sqm_rows:
-        cells = row.find_all("td")
-        if len(cells) > 1:
-            price_cell = cells[1].get_text(strip=True).replace(" ", "")
-            try:
-                price = float(price_cell)
-                total_price += price
-                count += 1
-            except ValueError:
-                continue
-
-    if count > 0:
-        average_price = total_price / count
-    else:
-        average_price = 0
-
-    return average_price
-
-
-def get_btc_price_in_eur():
-    btc_url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
-    exchange_rate_url = "https://api.exchangerate-api.com/v4/latest/USD"
-
-    # Get Bitcoin price in USD
-    btc_response = requests.get(btc_url).json()
-    btc_price_usd = float(btc_response["bpi"]["USD"]["rate"].replace(",", ""))
-
-    # Get USD to EUR conversion rate
-    exchange_rate_response = requests.get(exchange_rate_url).json()
-    usd_to_eur_rate = exchange_rate_response["rates"]["EUR"]
-
-    # Convert USD price to EUR
-    return btc_price_usd * usd_to_eur_rate
-
-
-def write_json_data(file_path, data):
-    with open(file_path, "a") as file:
-        json.dump(data, file)
-        file.write("\n")
-
-
-def get_prices_and_ratio():
-    os.makedirs("data", exist_ok=True)
-    if not os.path.exists("data/data.json"):
-        with open("data/data.json", "w") as f:
-            pass
-
-    data = []
-    if os.path.getsize("data/data.json") > 0:
-        with open("data/data.json", "r") as json_file:
-            data = [json.loads(line) for line in json_file if line.strip()]
-
-    now = datetime.utcnow()
-    if data:
-        last_entry = data[-1]
-        last_entry_time = datetime.strptime(last_entry["date"], "%d %b %Y")
-        if (now - last_entry_time).total_seconds() < 86400:
-            return
-
-    sqm_price = get_sqm_price_in_eur()
-    btc_price = get_btc_price_in_eur()
-    ratio = sqm_price / btc_price
-    dict_data = {
-        "date": now.strftime("%d %b %Y"),
-        "btc_price": btc_price,
-        "sqm_price": sqm_price,
-        "ratio": ratio,
-    }
-    write_json_data("data/data.json", dict_data)
+app = Flask(
+    __name__,
+    static_url_path="/static",
+    static_folder="static",
+    template_folder="templates",
+)
 
 
 @app.route("/data")
--- static/chart.js
+++ static/chart.js
@@ -1,72 +1,77 @@
 let priceChart = new Chart(
-    document.getElementById("priceChart").getContext("2d"),
-    {
-      type: "line",
-      data: {
-        labels: [],
-        datasets: [
-          {
-            backgroundColor: "rgba(247, 147, 26, 1.0)",
-            label: "Ratio sqm Price vs. BTC Price",
-            data: [],
-            borderColor: "rgba(247, 147, 26, 1.0)",
-            borderWidth: 2,
-            fill: false,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: "Time",
-              color: "#D8DEE9",
-              font: { size: 18 },
-            },
-            ticks: {
-              color: "rgba(216, 222, 233, 1.0)",
-              font: { size: 14 },
-            },
-            grid: { color: "rgba(216, 222, 233, 1.0)" },
-          },
-          y: {
-            title: {
-              display: true,
-              text: "Ratio",
-              color: "#D8DEE9",
-              font: { size: 18 },
-            },
-            ticks: {
-              color: "rgba(216, 222, 233, 1.0)",
-              font: { size: 14 },
-            },
-            grid: { color: "rgba(216, 222, 233, 1.0)" },
-            min: 0,
-          },
+  document.getElementById("priceChart").getContext("2d"),
+  {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          backgroundColor: "rgba(247, 147, 26, 1.0)",
+          label: "Ratio sqm Price vs. BTC Price",
+          data: [],
+          borderColor: "rgba(247, 147, 26, 1.0)",
+          borderWidth: 2,
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      elements: {
+        line: {
+          tension: 0.4, // smooth lines
         },
       },
-    }
-  );
-
-  function updateCharts() {
-    fetch("/data")
-      .then((response) => response.json())
-      .then((data) => {
-        // Clear existing chart data
-        priceChart.data.labels = [];
-        priceChart.data.datasets[0].data = [];
-
-        // Populate the chart with data from the JSON response
-        data.forEach((entry) => {
-          priceChart.data.labels.push(entry.date);
-          priceChart.data.datasets[0].data.push(entry.ratio);
-        });
-
-        // Update the chart
-        priceChart.update();
-      });
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: "Time",
+            color: "#D8DEE9",
+            font: { size: 18 },
+          },
+          ticks: {
+            color: "rgba(216, 222, 233, 1.0)",
+            font: { size: 14 },
+          },
+          grid: { color: "rgba(216, 222, 233, 1.0)" },
+        },
+        y: {
+          title: {
+            display: true,
+            text: "Ratio",
+            color: "#D8DEE9",
+            font: { size: 18 },
+          },
+          ticks: {
+            color: "rgba(216, 222, 233, 1.0)",
+            font: { size: 14 },
+          },
+          grid: { color: "rgba(216, 222, 233, 1.0)" },
+          min: 0,
+        },
+      },
+    },
   }
+);
 
-  // Update the chart every minute
-  setInterval(updateCharts, 6000);
+function updateCharts() {
+  fetch("/data")
+    .then((response) => response.json())
+    .then((data) => {
+      // Clear existing chart data
+      priceChart.data.labels = [];
+      priceChart.data.datasets[0].data = [];
+
+      // Populate the chart with data from the JSON response
+      data.forEach((entry) => {
+        priceChart.data.labels.push(entry.date);
+        priceChart.data.datasets[0].data.push(entry.ratio);
+      });
+
+      // Update the chart
+      priceChart.update();
+    });
+}
+
+// Update the chart every minute
+setInterval(updateCharts, 6000);
