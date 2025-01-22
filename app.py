07637f4ed9e8300e24b0917a9e50421c9373a7cb
tonytech83 <a.petrov.83@gmail.com>
2025-01-22 10:24:22 +0200

    fix the problem with gunicorn search for attribute 'app' in 'app' in main workflow

--- app.py
+++ app.py
@@ -53,8 +53,10 @@ def create_app() -> Flask:
     return app
 
 
+# Create an app instance for Gunicorn
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.logger.info(f"Starting server on port {port}")
     app.run(host="0.0.0.0", port=port)
